27c6decc0c4ddbbe64a380e068830795f8e29df9
JoshShabel <jshabel@purdue.edu>
2025-03-15 17:58:28 -0400

    Commit old WIP that's still viewable

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {HashRouter, Route, Routes} from "react-router-dom";
+import SubPage from "./SubPage.jsx";
+import Home from "./Home.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <HashRouter>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/numbers" element={<SubPage/>} />
+          </Routes>
+      </HashRouter>
   </StrictMode>,
 )
